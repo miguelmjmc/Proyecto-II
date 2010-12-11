315f9551c6d83eb029668b6ae8143582210a75f5
miguel malave <miguel.mjmc@gmail.com>
2010-12-11 00:08:35 -0430

    -añadido google map a index -simplificado index del administrador

---
+++ js/google_map.js
@@ -0,0 +1,25 @@
+
+
+function init_map() {
+    var var_location = new google.maps.LatLng(10.670282924574199,-63.250987976789474);
+
+    var var_mapoptions = {
+        center: var_location,
+        zoom: 18,
+        scrollwheel: false
+    };
+
+    var var_marker = new google.maps.Marker({
+        position: var_location,
+        map: var_map,
+        title:"Venice"});
+
+    var var_map = new google.maps.Map(document.getElementById("map-container"),
+        var_mapoptions);
+
+    var_marker.setMap(var_map);
+
+}
+
+google.maps.event.addDomListener(window, 'load', init_map);
+
